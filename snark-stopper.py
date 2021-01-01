41f1ec86c7c32d448eb38bc6206df18953e4cd81
c29r3 <ddzerosdc@gmail.com>
2021-01-01 21:48:20 +0300

    Changed worker fee format (nanomina --> MINA float)

--- snark-stopper.py
+++ snark-stopper.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-# Python 3.6+
 import logging
 import yaml
 import time
@@ -34,39 +32,52 @@ def parse_next_proposal_time():
         return "err"
 
 
+def parse_worker_pubkey():
+    daemon_status        = coda.get_daemon_status()
+    status_worker_pub    = daemon_status["daemonStatus"]["snarkWorker"]
+
+    if len(WORKER_PUB_KEY) == 55:
+        return WORKER_PUB_KEY
+    
+    elif status_worker_pub is not None and len(status_worker_pub) == 55:
+        return status_worker_pub
+
+    elif status_worker_pub is None:
+        logger.info(f'Worker public key is None\n'
+                    f'Automatically apply Block production key to {WORKER_PUB_KEY}')
+        
+        BLOCK_PROD_KEY = daemon_status["daemonStatus"]["blockProductionKeys"][0]
+        return BLOCK_PROD_KEY
+
+    else:
+        logger.fatal('Enter the worker public key in config.yml')
+        exit(1)
+    
+
 # Configure Logging
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='|%(asctime)s| %(message)s')
 logger = logging.getLogger(__name__)
 c = yaml.load(open('config.yml', encoding='utf8'), Loader=yaml.SafeLoader)
-logger.info("version 1.1")
-WORKER_PUB_KEY          = c["WORKER_PUB_KEY"]
-WORKER_FEE              = c["WORKER_FEE"]
-CHECK_PERIOD_SEC        = c["CHECK_PERIOD_SEC"]
-STOP_WORKER_FOR_MIN     = c["STOP_WORKER_FOR_MIN"]
-STOP_WORKER_BEFORE_MIN  = c["STOP_WORKER_BEFORE_MIN"]
-GRAPHQL_HOST            = c["GRAPHQL_HOST"]
-GRAPHQL_PORT            = c["GRAPHQL_PORT"]
+logger.info("version 1.2")
 
-coda = Client(graphql_host=GRAPHQL_HOST, graphql_port=GRAPHQL_PORT)
-daemon_status = coda.get_daemon_status()
+WORKER_PUB_KEY          = str(c["WORKER_PUB_KEY"])
+WORKER_FEE              = float(c["WORKER_FEE"])
+CHECK_PERIOD_SEC        = int(c["CHECK_PERIOD_SEC"])
+STOP_WORKER_FOR_MIN     = int(c["STOP_WORKER_FOR_MIN"])
+STOP_WORKER_BEFORE_MIN  = int(c["STOP_WORKER_BEFORE_MIN"])
+GRAPHQL_HOST            = str(c["GRAPHQL_HOST"])
+GRAPHQL_PORT            = int(c["GRAPHQL_PORT"])
+# MINA --> nanomina
+WORKER_FEE              = int(WORKER_FEE * 1e9)
 
-if type(WORKER_PUB_KEY) is not str or len(WORKER_PUB_KEY) != 55:
-    try:
-        WORKER_PUB_KEY = daemon_status["daemonStatus"]["snarkWorker"]
-        BLOCK_PROD_KEY = daemon_status["daemonStatus"]["blockProductionKeys"][0]
+try:
+    coda = Client(graphql_host=GRAPHQL_HOST, graphql_port=GRAPHQL_PORT)
 
-        if WORKER_PUB_KEY is None:
-            logger.info(f'Worker public key is None\n'
-                        f'Automatically apply Block production key to {WORKER_PUB_KEY}')
-            WORKER_PUB_KEY = BLOCK_PROD_KEY
+except:
+    logger.fatal(f'Can\'t connect to graphql {GRAPHQL_HOST}:{GRAPHQL_PORT}.\n'
+                 f'Check troubleshooting manual - https://github.com/c29r3/mina-snark-stopper#troubleshooting')
 
-        worker_manager(mode="on")
-        logger.info(f'SNARK worker: {WORKER_PUB_KEY}\n'
-                    f'SNARK work fee: {WORKER_FEE}')
-
-    except Exception as workerAddrErr:
-        logger.exception(f'Can\'t get worker public key. Is it running?')
-        exit(1)
+WORKER_PUB_KEY = parse_worker_pubkey()
 
 logger.info(f'Worker public key is: {WORKER_PUB_KEY}\n'
             f'Worker fee:           {WORKER_FEE}\n'
@@ -84,14 +95,23 @@ while True:
         time_to_wait = str(timedelta(seconds=int(next_proposal - time.time())))
         logger.info(f'Next proposal via {time_to_wait}')
         if next_proposal-time.time() < STOP_WORKER_BEFORE_MIN*60:
-            worker_on = worker_manager(mode="off")
-            logger.info(worker_on)
+            worker_off = worker_manager(mode="off")
+            logger.info(worker_off)
 
             logger.info(f'Waiting {STOP_WORKER_FOR_MIN} minutes')
             time.sleep(60 * STOP_WORKER_FOR_MIN)
 
-            worker_off = worker_manager(mode="on")
-            logger.info(worker_off)
+            worker_on = worker_manager(mode="on")
+            logger.info(worker_on)
+
+        else:
+            daemon_status = coda.get_daemon_status()
+            worker_pub_key = daemon_status["daemonStatus"]["snarkWorker"]
+            # If WORKER OFF and enought time --> WORKER ON
+            if worker_pub_key is None:
+                worker_on = worker_manager(mode="on")
+                logger.info(f'Autostart worker, next proposal in {time_to_wait}')
+
         time.sleep(CHECK_PERIOD_SEC)
 
     except (TypeError, Exception) as parseErr:
